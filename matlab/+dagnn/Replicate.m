d33cdde11147e398974c4f193f161d1abf405fb6
Peiyun Hu <peiyunh.ph@gmail.com>
2016-12-15 16:41:06 -0500

    save

--- matlab/+dagnn/Replicate.m
+++ matlab/+dagnn/Replicate.m
@@ -1,19 +1,24 @@
 classdef Replicate < dagnn.ElementWise
     properties 
-        childIds = [];
+        childIds = {};
         parentIds = [];
     end
 
     methods
         function outputs = forward(obj, inputs, params)
-            assert(numel(inputs)==1); 
+            assert(numel(inputs)==1);
             outputs{1} = inputs{1}(:,:,obj.parentIds,:);
         end
 
         function [derInputs, derParams] = backward(obj, inputs, params, derOutputs)
-            derInputSlices = cellfun(@(I)(sum(derOutputs{1}(:,:,I,:),3)),...
+            % NOTE sum(a(:,[],:),2) returns all zeros with correct shape
+            %      even when a(:,[],:) is an empty matrix
+            derInputSlices = cellfun(@(I)(sum(derOutputs{1}(:,:,I,:),3)), ...
                                      obj.childIds, 'UniformOutput', false);
-            derInputs{1} = cat(3, derInputSlices{:});
+            dI = cat(3, derInputSlices{:});
+            empty_idx = cellfun(@isempty, obj.childIds);
+            dI(:,:,empty_idx,:) = 0 .* dI(:,:,empty_idx,:);
+            derInputs{1} = dI;
             derParams = {};
         end
 
